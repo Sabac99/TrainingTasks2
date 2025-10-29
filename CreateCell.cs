465ab348aebb4e8c7aee1d5ce8a17b8bef0916ab
Sabac99 <kalbosaVkysnaya96@gmail.com>
2025-10-29 15:53:44 +0300

    task3 upld need to fix

---
+++ CreateCell.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingTasks2
+{
+    internal class CreateCell
+    {
+        Random random = new Random();
+        public int cellX;
+        public int cellY;
+        public (int, int) getCellFromComp(int size, List<(int,int)> usedCells)
+        {
+            do
+            {
+                cellX = random.Next(1, size + 1);
+                cellY = random.Next(1, size + 1);
+            }
+            while (usedCells.Contains((cellX, cellY)));
+
+            return (cellX, cellY);
+        }
+        public (int, int) createCellsFromPlayer(int size, List<(int, int)> usedCells)
+        {
+            bool inputCorrect = false;
+            while (inputCorrect != true)
+            {
+                Console.WriteLine("Введите строку начиная с 1");
+                if (int.TryParse(Console.ReadLine(), out int playerCellX) && playerCellX >= 1 && playerCellX <= size)
+                {
+                    Console.WriteLine("Введите столбец начиная с 1");
+                    if (int.TryParse(Console.ReadLine(), out int playerCellY) && playerCellY >= 1 && playerCellY <= size)
+                    {
+                        if (!usedCells.Contains((playerCellX, playerCellY)))
+                        {
+                            inputCorrect = true;
+                            return (playerCellX, playerCellY);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ячейка уже была введена");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка ввода");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода");
+                }
+            }
+            return (0, 0);
+
+        }
+    }
+}
---
+++ FieldCreator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingTasks2
+{
+    internal class FieldCreator
+    {
+        public string[,] array;
+        public int size;
+        public FieldCreator(int size)
+        {
+            array = new string[size,size];
+            this.size = size;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    array[i, j] = "o";
+                }
+            }
+        }
+        public void PrintField()
+        {
+            for (int i = 0; i < this.size; i++)
+            {
+                for (int j = 0; j < this.size; j++)
+                {
+                    Console.Write(this.array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
